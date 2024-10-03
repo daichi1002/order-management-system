35e6186cb021cf59ec007f74a464c55c08f1ca2f
daichi.nagano <d.nagano1002@gmail.com>
2024-10-04 08:38:12 +0900

    add UpdateNumber func test

--- backend/test/repository/counter_repository_test.go
+++ backend/test/repository/counter_repository_test.go
@@ -1,6 +1,7 @@
 package repository_test
 
 import (
+	"errors"
 	"regexp"
 	"testing"
 	"time"
@@ -11,6 +12,7 @@ import (
 	"github.com/daichi1002/order-management-system/backend/internal/util"
 	"github.com/daichi1002/order-management-system/backend/test"
 	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
 )
 
 func TestGetNextNumber(t *testing.T) {
@@ -58,6 +60,72 @@ func TestGetNextNumber(t *testing.T) {
 				assert.Equal(t, tc.expectedCounter.Date, counter.Date)
 				assert.Equal(t, tc.expectedCounter.Number, counter.Number)
 
+			}
+
+			err = mock.ExpectationsWereMet()
+			assert.NoError(t, err)
+		})
+	}
+}
+
+func TestUpdateNumber(t *testing.T) {
+	db, mock, closeDB := test.SetupTestDB(t)
+	defer closeDB()
+
+	testCases := []struct {
+		name        string
+		counter     *model.Counter
+		expectError bool
+		mockSetup   func(sqlmock.Sqlmock)
+	}{
+		{
+			name: "成功：データが更新できる",
+			counter: &model.Counter{
+				ID:     1,
+				Date:   "2023-10-04",
+				Number: 11,
+			},
+			expectError: false,
+			mockSetup: func(mock sqlmock.Sqlmock) {
+				mock.ExpectBegin()
+				mock.ExpectExec(regexp.QuoteMeta(`UPDATE "counters" SET "date"=$1,"number"=$2 WHERE "id" = $3`)).
+					WithArgs("2023-10-04", 11, 1).
+					WillReturnResult(sqlmock.NewResult(1, 1))
+				mock.ExpectCommit()
+			},
+		},
+		{
+			name: "失敗：データベースエラー",
+			counter: &model.Counter{
+				ID:     1,
+				Date:   "2023-10-04",
+				Number: 11,
+			},
+			expectError: true,
+			mockSetup: func(mock sqlmock.Sqlmock) {
+				mock.ExpectBegin()
+				mock.ExpectExec(regexp.QuoteMeta(`UPDATE "counters" SET "date"=$1,"number"=$2 WHERE "id" = $3`)).
+					WithArgs("2023-10-04", 11, 1).
+					WillReturnError(errors.New("database error"))
+				mock.ExpectRollback()
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.mockSetup(mock)
+
+			repo := repository.NewCounterRepository(db)
+
+			err := db.Transaction(func(tx *gorm.DB) error {
+				return repo.UpdateNumber(tx, tc.counter)
+			})
+
+			if tc.expectError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
 			}
 
 			err = mock.ExpectationsWereMet()
